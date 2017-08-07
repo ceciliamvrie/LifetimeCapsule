e41095de953866b9b6978a0ea350df724d902bd8
Eric F <ericdfanning@gmail.com>
2017-08-07 17:43:49 -0500

    added start: nodemon server.js to package.json, Landing page now toggling signin/signup, auth factory added

---
+++ client/services/auth.js
@@ -0,0 +1,16 @@
+angular.module('landing-page')
+.factory('Auth', function() {
+
+  const signin = function(obj, cb) {
+
+  };
+
+  const signup = function(obj, cb) {
+  	
+  }
+  
+  return {
+    signin: signin,
+    signup: signup
+  };
+})
