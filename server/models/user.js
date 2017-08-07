8e6bdc95ac6a32a2fe2434443c06cee3366f8911
Joshua Nance <joshua.h.nance@gmail.com>
2017-08-07 17:46:19 -0500

    Add unique email field to users

--- server/models/user.js
+++ server/models/user.js
@@ -9,6 +9,11 @@ const userSchema = new Schema({
     type: String,
     required: true
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
   password: {
     type: String,
     required: true
