a2d84c4830b09fc776830216ec3b3de6bdba941a
ceciliamvrie <cecilia.mvrie@gmail.com>
2017-08-07 21:58:36 -0500

    Set up routes for views

--- server/server.js
+++ server/server.js
@@ -1,9 +1,25 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
-app.use(express.static(__dirname + '/../client'))
-app.use(express.static(__dirname + '/../node_modules'))
+app.use(express.static('client'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/home.html'))
+})
+
+app.get('/create', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/templates/create.html'))
+})
+
+app.get('/landing', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/templates/landing.html'))
+})
+
+app.get('/view', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/templates/view.html'))
+})
 
 app.get('/capsules', (req, res) => {
   res.send('The capsules will be here')
