e3547d03fecd196ae68bb53f87979835e5220b10
Joshua Nance <joshua.h.nance@gmail.com>
2017-08-09 18:48:23 -0500

    add permissions for other http requests

--- server/server.js
+++ server/server.js
@@ -15,8 +15,9 @@ app.use(bodyParser.json());
 app.use(express.static('client'));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 })
 
