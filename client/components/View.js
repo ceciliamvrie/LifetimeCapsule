0c601f63c601c10c0f7eda968a6d46d7f6ef4031
Eric F <ericdfanning@gmail.com>
2017-08-12 21:51:10 -0500

    View modal now displays details dynamically in the modal. Removed console logs. Added CSS to buttons

--- client/components/View.js
+++ client/components/View.js
@@ -1,8 +1,31 @@
 angular.module('app')
 .controller('ViewCtrl', function($scope, Caps) {
-  this.viewDetails = () => {
-  	console.log('view details was clicked!!!')
+  this.viewDetails = (cap) => {
+  	$('.modalBodyBorder').on('click', function(event) {
+  		$scope.chosenIndex = event.currentTarget.id;
+
+	    $('#viewModal').html(
+	    	`
+	       <div class="modal-dialog" id="viewModalDialog">
+			  <div class="modal-content" id="viewModalContent"> 
+			    <div class="modal-header">
+			      <button type="button" class="close" data-dismiss="modal">&times;</button>
+			      <h4 class="modal-title">${cap.capsuleName}</h4>
+			    </div>
+			    <div class="viewModal-body" id="viewModalBody">
+		     
+     	    	  <h4>${cap.contents[$scope.chosenIndex].name}</h4>
+     	    	  <p id="viewDetails">${cap.contents[$scope.chosenIndex].input}</p>
+			    
+			    </div>
+			  </div>
+			</div>
+		  `
+	    );
+
+  	})
   }
+
   this.viewCapsule = (cap) => {
     $('#viewModal').html(
     	`
@@ -21,12 +44,13 @@ angular.module('app')
 
     for (var i = 0; i < cap.contents.length; i++) {
       $('.viewModal-body').append(
-      	  `<div id="modalBodyBorder" ng-click="$ctrl.viewDetails">
-    	    <h4>${cap.contents[i].name}</h4>
-    	    <p class="momentoInput">${cap.contents[i].input}</p>
+      	  `<div class="modalBodyBorder" id="${i}">
+	    	  <h4>${cap.contents[i].name}</h4>
+	    	  <p class="momentoInput">${cap.contents[i].input}</p>
     	  </div>`
-        )
+       )
     }
+
   }
 })
 .component('viewPage', {
