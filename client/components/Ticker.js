1c8cef99c59cdad2dbcac5b95a6666ed34a2ff42
Joshua Nance <joshua.h.nance@gmail.com>
2017-08-14 09:19:25 -0500

    Fixes conflict

--- client/components/Ticker.js
+++ client/components/Ticker.js
@@ -7,15 +7,8 @@ angular.module('app')
     let day = dateString[2];
     let year = dateString[0];
 
-
-  let dateString = this.capsule.unearthDate.split('T')[0].split('-');
-  let month = dateString[1];
-  let day = dateString[2];
-  let year = dateString[0];
-
-  this.unearthMessage = `Capsule unearthed on ${month}/${day}/${year}`;
+    this.unearthMessage = `You may open this capsule on ${month}/${day}/${year}`;
   }
-
 })
 
 .component('ticker', {
