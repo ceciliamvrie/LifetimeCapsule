f2d129e0bd9f64263789a5e7afe1797ff848e996
Joshua Nance <joshua.h.nance@gmail.com>
2017-08-13 20:31:00 -0500

    Add unearthDate to capsules view using new component ticker

---
+++ client/components/Ticker.js
@@ -0,0 +1,21 @@
+angular.module('app')
+
+.controller('TickerCtrl', function() {
+
+  let dateString = this.capsule.unearthDate.split('T')[0].split('-');
+  let month = dateString[1];
+  let day = dateString[2];
+  let year = dateString[0];
+
+  this.unearthMessage = `You may open this capsule on ${month}/${day}/${year}`;
+})
+
+.component('ticker', {
+  controller: 'TickerCtrl',
+
+  bindings: {
+    capsule: '<'
+  },
+
+  templateUrl: '../templates/ticker.html'
+})
